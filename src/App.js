0dcf6b5dcb00918563cb9eba3f8ed044ad19eef7
Sammed Dadape <samdadape@gmail.com>
2022-09-19 18:22:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "😄": "Smiling",
+  "😧": "Disbelife",
+  "😡": "Angry",
+  "😴": "Sleepy",
+  "🤒": "Sick"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiChangeHandler(event) {
+    const userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "Sorry! we don't have this in our Database";
+    }
+
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    console.log(emoji);
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Emoji Interpreter! </h1>
+      <input onChange={emojiChangeHandler} />
+      <h2> {meaning} </h2>
+      <h3>Emojis We Know</h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            key={emoji}
+            style={{ fontSize: "2rem", padding: "1rem", cursor: "pointer" }}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
+// VISER--
+// VIEW -- INTERACT -- STATE IN EVENT HANDLER -- RENDER
